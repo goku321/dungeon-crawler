3812dce5b7a01346ef880903eb3857eb254b3565
Deepak <deepak.sah@nutanix.com>
2023-05-13 15:20:06 +0530

    Update player movements

--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,6 @@
 use crate::prelude::*;
 
-struct Player {
+pub struct Player {
     pub position: Point
 }
 
@@ -19,5 +19,22 @@ impl Player {
             BLACK,
             to_cp437('@'),
         );
+    }
+
+    pub fn update(&mut self, ctx: &mut BTerm, map: &Map) {
+        if let Some(key) = ctx.key {
+            let delta = match key {
+                VirtualKeyCode::Left => Point::new(-1, 0),
+                VirtualKeyCode::Right => Point::new(1, 0),
+                VirtualKeyCode::Up => Point::new(0, -1),
+                VirtualKeyCode::Down => Point::new(0, 1),
+                _ => Point::zero()
+            };
+
+            let new_position = self.position + delta;
+            if map.can_enter_tile(new_position) {
+                self.position = new_position
+            }
+        }
     }
 }
